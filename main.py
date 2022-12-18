10cfb42710dfff452b8e3d89f53d3714c6c76a77
pedrodemaia <pedrodemaia@gmail.com>
2022-12-18 17:35:31 -0300

    get summary table

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
+'''
+This module prints a kart race evolution from mylaptime.com.
+'''
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    race_link = 'http://www.mylaptime.com/laptime/clientes/214V20106819C9780G1X1P108/results/r3.html?evt=11226&epg=6018'
+    driver = webdriver.Chrome()
+    driver.get(race_link)
+    driver.implicitly_wait(1.0)
+    race_link = driver.find_element(By.XPATH, "/html/body/div[2]/table/tbody/tr[3]/td[2]/a")
+    race_link.click()
+    driver.implicitly_wait(1.0)
+    summary_table = driver.find_element(By.XPATH, "/html/body/div[3]/table/tbody")
+
+    spec_name = []
+    spec_item = []
+    for row in summary_table.find_elements(By.XPATH, ".//tr"):
+        spec_name.append(row.find_element(By.XPATH, './th').get_attribute('textContent'))
+        spec_item.append(row.find_element(By.XPATH, './td/span').get_attribute('textContent'))
+
+    df = pd.DataFrame({"Spec_Name": spec_name, "Spec_Title": spec_item})
+    a = 1.0
+
