4545bd3f7c3f944036005a067dd7bfd91648f57f
pedrodemaia <pedrodemaia@gmail.com>
2023-04-01 10:54:53 -0300

    Add race link argument to main

--- main.py
+++ main.py
@@ -6,10 +6,18 @@ from data_import.import_data import download_race_data
 from data_process.process_race_data import process_data
 from data_print.print_race_evolution import create_gif
 
-if __name__ == '__main__':
-    race_link = 'http://www.mylaptime.com/laptime/clientes/214V20106819C9780G1X1P108/results/r3.html?evt=11226&epg=6018'
+import argparse
 
-    (summary_df, detailed_df) = download_race_data(race_link)
+if __name__ == '__main__':
+
+    default_link = 'http://www.mylaptime.com/laptime/clientes/214V20106819C9780G1X1P108/results/r3.html?evt=11303&epg=6512'
+
+    parser = argparse.ArgumentParser(description='Kart timer race link')
+    parser.add_argument('race_link', type=str, default=default_link, help='Race link')
+
+    args = parser.parse_args()
+
+    (summary_df, detailed_df) = download_race_data(args.race_link)
     times, cumulative, positions, fast_laps, speed = process_data(summary_df, detailed_df)
     create_gif(positions, speed)
 
